0e67a4a815e97d2f50c92b0ae60b88d81abd41f3
RRoager <rasmus-roager@hotmail.com>
2021-12-20 10:31:53 +0100

    DateActivity er stylet færdigt med billede osv.

--- app/src/main/java/com/roager/moodcalenderapp/DateActivity.java
+++ app/src/main/java/com/roager/moodcalenderapp/DateActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.ImageView;
 import android.widget.TextView;
 
